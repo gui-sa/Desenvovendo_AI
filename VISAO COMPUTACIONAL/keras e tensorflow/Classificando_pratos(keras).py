293037bb0de3bdd36da6469ce8cd2e8a07d77448
Guilherme Salomao <guime.sa9@gmail.com>
2020-04-20 13:39:35 -0300

    Comecando a programar o classificador de imagens - ja fiz o preparador de dados... em pleno funcionamento.

---
+++ VISAO COMPUTACIONAL/keras e tensorflow/Classificando_pratos(keras).py
@@ -0,0 +1,116 @@
+#O objetivo é criar um programa que é treinado em cima de um data base de pratos sujos/pratos limpos
+#O usuario escolhe entre treinar a rede e utilizar a rede ja treinada
+#OBVIO, que ainda nao vai possuir interface grafica.
+#De inicio vou usar o data set ja montado : pardais!!
+
+import tensorflow as tf
+tf.compat.v1.disable_eager_execution()#TensorFlow's eager execution is an imperative programming environment that evaluates operations immediately, without building graphs
+import keras
+import numpy as np
+import sklearn as sk
+import cv2 as cv
+import os
+
+#Preparando os dados de treinamento =============================================================================================
+
+train_path = '/home/salomao/Desktop/Detector_images/Pardal_Treinamento'#Dretorio de treinamento
+labels = os.listdir(train_path)#As legendas das imagens.
+print('\nTraining Data:\n')
+print('\nO valor 0 se refere ' + str(labels[0]))
+print('\nO valor 1 se refere ' + str(labels[1]) + '\n')
+
+img_label_1 = os.path.join(train_path,str(labels[0]))#Entro no diretorio da primeira lista
+ls = os.listdir(img_label_1)#Criando uma lista com o que tem dentro da pasta, para podermos acessar de forma facil
+
+label_1_array = []#Criando uma lista vazia
+for i in ls:#Pego o nome de um arquivo da lista ls 
+    pat = os.path.join(img_label_1,str(i))#Dou join nele
+    img = cv.imread(pat)#leio ele como imagem
+    img = np.resize(img,(500,500,3))#Dou rezise - padronizo as imagens
+    label_1_array.append(img)#Adiciona na lista
+label_1_array = np.array(label_1_array)#Deposis que tudo foi adicionado, passo a lista para um array
+
+#Preciso criar um array contendo o label respectivo... ele deve conter o shape do numero de imagens
+
+label_array_from_label_1_array = np.zeros((len(label_1_array)))
+label_array_train = label_array_from_label_1_array
+train_data = label_1_array
+
+#Juntando os dados dp outro
+
+img_label_2 = os.path.join(train_path,str(labels[1]))#Entro no diretorio da primeira lista
+ls = os.listdir(img_label_2)#Criando uma lista com o que tem dentro da pasta, para podermos acessar de forma facil
+
+label_1_array = []#Criando uma lista vazia
+for i in ls:#Pego o nome de um arquivo da lista ls 
+    pat = os.path.join(img_label_2,str(i))#Dou join nele
+    img = cv.imread(pat)#leio ele como imagem
+    img = np.resize(img,(500,500,3))#Dou rezise - padronizo as imagens
+    label_1_array.append(img)#Adiciona na lista
+label_1_array = np.array(label_1_array)#Deposis que tudo foi adicionado, passo a lista para um array
+
+#Preciso criar um array contendo o label respectivo... ele deve conter o shape do numero de imagens
+
+label_array_from_label_1_array = np.ones((len(label_1_array)))
+    
+train_data=np.append(train_data,label_1_array,axis=0)#Juntando tudo das imagens em um unico numpy. Este é o input
+label_array_train= np.append(label_array_train,label_array_from_label_1_array,axis=0)#Juntando tudo das imagens em um unico numpy. Este é o target_true
+
+
+
+#Preparando os dados de validacao ===============================================================================================
+
+
+val_path = '/home/salomao/Desktop/Detector_images/Pardal_Validacao'#Dretorio de treinamento
+labels = os.listdir(val_path)#As legendas das imagens.
+print('\nValidation data:\n')
+print('\nO valor 0 se refere ' + str(labels[0]))
+print('\nO valor 1 se refere ' + str(labels[1]) + '\n')
+
+img_label_1 = os.path.join(val_path,str(labels[0]))#Entro no diretorio da primeira lista
+ls = os.listdir(img_label_1)#Criando uma lista com o que tem dentro da pasta, para podermos acessar de forma facil
+
+label_1_array = []#Criando uma lista vazia
+for i in ls:#Pego o nome de um arquivo da lista ls 
+    pat = os.path.join(img_label_1,str(i))#Dou join nele
+    img = cv.imread(pat)#leio ele como imagem
+    img = np.resize(img,(500,500,3))#Dou rezise - padronizo as imagens
+    label_1_array.append(img)#Adiciona na lista
+label_1_array = np.array(label_1_array)#Deposis que tudo foi adicionado, passo a lista para um array
+
+#Preciso criar um array contendo o label respectivo... ele deve conter o shape do numero de imagens
+
+label_array_from_label_1_array = np.zeros((len(label_1_array)))
+label_array_val = label_array_from_label_1_array
+val_data = label_1_array
+
+#Juntando os dados dp outro
+
+img_label_2 = os.path.join(val_path,str(labels[1]))#Entro no diretorio da primeira lista
+ls = os.listdir(img_label_2)#Criando uma lista com o que tem dentro da pasta, para podermos acessar de forma facil
+
+label_1_array = []#Criando uma lista vazia
+for i in ls:#Pego o nome de um arquivo da lista ls 
+    pat = os.path.join(img_label_2,str(i))#Dou join nele
+    img = cv.imread(pat)#leio ele como imagem
+    img = np.resize(img,(500,500,3))#Dou rezise - padronizo as imagens
+    label_1_array.append(img)#Adiciona na lista
+label_1_array = np.array(label_1_array)#Deposis que tudo foi adicionado, passo a lista para um array
+
+#Preciso criar um array contendo o label respectivo... ele deve conter o shape do numero de imagens
+
+label_array_from_label_1_array = np.ones((len(label_1_array)))
+    
+val_data=np.append(val_data,label_1_array,axis=0)#Juntando tudo das imagens em um unico numpy. Este é o input
+label_array_val= np.append(label_array_val,label_array_from_label_1_array,axis=0)#Juntando tudo das imagens em um unico numpy. Este é o target_true
+
+
+
+print ('\nAVISO: se os valores dos nomes treinamento de validacao nao coincidirem, os dados nao serao validos.\n')
+
+
+
+
+
+
+
