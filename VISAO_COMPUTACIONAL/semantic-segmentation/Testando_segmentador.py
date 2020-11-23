d3b3d8df973b3eb0602a298891d804a401f780be
Guilherme Salomao <guime.sa9@gmail.com>
2020-11-23 15:04:12 -0300

    Testei o meu modelo unet-1 para todas as imagens, e ainda por cima testei a mesma mascara aumentando o tamanho da mascara

---
+++ VISAO_COMPUTACIONAL/semantic-segmentation/Testando_segmentador.py
@@ -0,0 +1,81 @@
+#Autor: Guilherme SalomÃ£o Agostini 
+#Email: guime.sa9@gmail.com
+#Referecias importantes:
+
+
+#%% Prepare paths of input images and target segmentation masks
+import os
+import cv2 as cv
+import numpy as np
+
+img_size = (160, 160)
+
+path_img = "/home/salomao/Desktop/insulators-dataset/jpg"
+path_ann = "/home/salomao/Desktop/insulators-dataset/tiff"
+
+list_img = os.listdir(path_img)
+
+images =  []
+original = []
+for img_idx in list_img:
+    path_img_idx = os.path.join(path_img, img_idx)
+    img  =  cv.imread(path_img_idx)
+    original.append(img)
+    img = cv.resize(img,img_size,interpolation = cv.INTER_AREA)
+    img = img/255.0
+    images.append(img)
+
+
+images = np.array(images)
+#%% Loadando modelo
+from tensorflow import keras
+import numpy as np
+
+model = keras.models.load_model("/home/salomao/Desktop/insulators.h5")
+pred = model.predict(images)
+
+#%% Checando a segmentação de todas as imagens do dataset
+for i in range(len(images)):
+    res = pred[i]
+    res = np.argmax(res,-1)
+    res = np.expand_dims(res, axis=-1)
+    masked = np.multiply(images[i],res)
+    cv.imshow("imagem original " + str(i),images[i])
+    cv.waitKey()
+    cv.destroyAllWindows()
+    cv.imshow("Segmented " + str(i),masked)
+    ask = cv.waitKey()
+    cv.destroyAllWindows()
+    if ask==113:
+        break
+
+
+#%% Aproveitando o mesmo predict para a imagem em alta resolucao
+i = 0
+res = pred[i]
+res = np.argmax(res,-1)
+res = np.float32(res)
+size =( original[i].shape[1],original[i].shape[0])
+res = cv.resize(res, size, interpolation = cv.INTER_AREA)
+res = np.round(res)
+res = np.expand_dims(res, axis=-1)
+masked = np.multiply(original[i]/255.0,res)
+cv.imshow("Segmented Huge",masked)
+ask = cv.waitKey()
+cv.destroyAllWindows()
+
+#%% Aproveitando o mesmo predict para a imagem em alta resolucao (controlando a resolucao)
+i = 0
+res = pred[i]
+res = np.argmax(res,-1)
+res = np.float32(res)
+size =(800,800)
+res = cv.resize(res, size, interpolation = cv.INTER_AREA)
+res = np.round(res)
+res = np.expand_dims(res, axis=-1)
+img = cv.resize(original[i], size, interpolation = cv.INTER_AREA)
+masked = np.multiply(img/255.0,res)
+cv.imshow("Segmented Controlled",masked)
+ask = cv.waitKey()
+cv.destroyAllWindows()
+
--- VISAO_COMPUTACIONAL/semantic-segmentation/utilities.py
+++ VISAO_COMPUTACIONAL/semantic-segmentation/utilities.py
@@ -5,11 +5,10 @@
 
 #%% Prepare paths of input images and target segmentation masks
 import os
-from tensorflow import keras
 import cv2 as cv
 import numpy as np
 
-
+img_size = (160, 160)
 
 path_img = "/home/salomao/Desktop/insulators-dataset/jpg"
 path_ann = "/home/salomao/Desktop/insulators-dataset/tiff"
@@ -21,17 +20,25 @@ list_ann = os.listdir(path_ann)
 list_ann.sort()
 list_ann = sorted(list_ann,key=len)
 
+images =  []
+annotation= []
 for img_idx,ann_idx in zip(list_img,list_ann):
     path_img_idx = os.path.join(path_img, img_idx)
     path_ann_idx = os.path.join(path_ann, ann_idx)
     img  =  cv.imread(path_img_idx)
-    ann =   cv.imread(path_ann_idx)
+    ann =   cv.imread(path_ann_idx, cv.IMREAD_GRAYSCALE)
     _, ann = cv.threshold(ann,1,1,cv.THRESH_BINARY)
+    img = cv.resize(img,img_size,interpolation = cv.INTER_AREA)/255.0
+    ann = cv.resize(ann,img_size,interpolation = cv.INTER_AREA)  
     
-    img = np.multiply(img,ann) 
-    img_masked = cv.resize(img,(800,800),interpolation = cv.INTER_AREA)
-    cv.imshow("preview",img_masked)
-    cv.waitKey()
-    cv.destroyAllWindows()
-
-
+    images.append(img)
+    annotation.append(ann)
+    
+    # img = np.multiply(img,ann) 
+    # cv.imshow("preview",img)
+    # cv.waitKey()
+    # cv.destroyAllWindows()
+    
+images = np.array(images)
+annotation = np.array(annotation)
+annotation = np.reshape(annotation,(len(annotation),img_size[0],img_size[1], -1))
