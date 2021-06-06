b61817d67e58f9305dfeeebb36d41ab7c8c1d075
Guilherme Salomao <guime.sa9@gmail.com>
2021-06-06 20:02:54 -0300

    Primeira versao ja em funcionamentogit add .

--- Tools/segmentation_data_generator.py
+++ Tools/segmentation_data_generator.py
@@ -4,6 +4,7 @@
 #Email: guime.sa9@gmail.com
 
 #%% Preparando os caminhos e o ordenamentos dos nomes
+
 import os
 import cv2 as cv
 import numpy as np
@@ -52,7 +53,21 @@ images = np.array(images)#passa para vetor de numpy
 annotation = np.array(annotation)
 annotation = np.reshape(annotation,(len(annotation),img_size[0],img_size[1], -1))#expandindo uma nova dimensao para que seja aplicavel no keras
 
+#%% funcao que inverte a mascara binaria
+
+#Para usá-la:
+#ann =   cv.imread("/home/salomao/Desktop/generated_masks/0.jpg", cv.IMREAD_GRAYSCALE)
+#ann = invert_mask(ann)
+#cv.imshow("teste",ann)
+def invert_mask(ann):
+    ann = np.round(ann)
+    ann = 1 - ann
+    ann = ann
+    return ann
+
+
 #%% Inicio augmentation
+
 from tensorflow import keras
 import numpy as np
 datagen_args = dict(    
@@ -69,26 +84,43 @@ batch_size = 5
 seed = 1#a randomizacao precisa ser identicas, nome seed, para que as imagens se casem apos a augmentacao
 teste_num = 15
 
-#%% Testando augmentation
+#%% Caminho das imagens que servirao de background.
+path_background = "/home/salomao/Desktop/background/"
+
+list_background = os.listdir(path_background)#lista todas as imagens que estao nesse path
+for indx in range(len(list_background)):
+    list_background[indx] = os.path.join(path_background,list_background[indx])
+
+
+#%% Obtendo augmentation, casando as imagens e povoando a pasta
+import random 
 it1 = img_datagen.flow(images , batch_size=batch_size, seed=seed)
 it2 = mask_datagen.flow(annotation , batch_size=batch_size, seed=seed)
 
 for i in range(teste_num):
     img_batch = it1[i]
     ann_batch = it2[i]
-    for idx in range(batch_size):
+    for idx in range(batch_size): 
         img = img_batch[idx]
         ann = ann_batch[idx]
-        cv.imwrite("/home/salomao/Desktop/generated_png/" + str(idx + i*batch_size) + ".jpg", img*255)
+        rand = random.randrange(0,(len(list_background)))
+        background  =  cv.imread(list_background[rand])#leitura da imagem em rgb
+        background = cv.resize(background,img_size,interpolation = cv.INTER_AREA)
+        # cv.imshow("background_original",background)
+        # ask = cv.waitKey()
+        background = np.multiply(background,invert_mask(ann))
+        background = np.uint8(background)
+        img = np.multiply(img,ann)
+        img = img*255
+        img = np.uint8(img)
+        img_final = background + img
+        img_final = np.uint8(img_final)
+        cv.imwrite("/home/salomao/Desktop/generated_png/" + str(idx + i*batch_size) + ".jpg", img_final)
         cv.imwrite("/home/salomao/Desktop/generated_masks/" + str(idx + i*batch_size) + ".jpg", ann*255)
         #=============DEBUG
-        # teste = np.multiply(img,ann)
         # cv.imshow("Imagem Original",img)
-        # cv.waitKey()
-        # cv.destroyAllWindows()
-        # cv.imshow("preview",teste)
-        # cv.waitKey()
+        # ask = cv.waitKey()
+        # cv.imshow("Imagem montada",img_final)
+        # ask = cv.waitKey()
         # cv.destroyAllWindows()       
-        # if ask==113:#sai se precionar ´q´
-        #   break 
-        
+
